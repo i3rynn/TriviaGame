97a390fd051ccb4c86ae72b9452322082ce82e35
Brynn Chapman <brynn_m_chapman@outlook.com>
2019-10-26 20:09:31 -0500

    Updated files again.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,30 @@
 document.getElementById("after_submit").style.display = "none";
 document.getElementById("questionsDiv").style.display = "none";
 document.getElementById("startPage").style.display = "block";
-​
+
 var timerCounter = 180;
 var timer;
-function runTimer(){
-timer = setInterval(updateTimer, 1000);
+function runTimer() {
+  timer = setInterval(updateTimer, 1000);
 }
-​
-function updateTimer(){
+
+function updateTimer() {
   document.getElementById("time").innerText = timerCounter;
   timerCounter--;
-​
-  if(timerCounter == 0){
+
+  if (timerCounter == 0) {
     clearInterval(timer);
-  check();
-  document.getElementById("after_submit").style.display = "block";
-  document.getElementById("questionsDiv").style.display = "none";
-  document.getElementById("startPage").style.display = "none";
+    check();
+    console.log("Time is up!");
+    document.getElementById("after_submit").style.display = "block";
+    document.getElementById("questionsDiv").style.display = "none";
+    document.getElementById("startPage").style.display = "none";
   }
-​
+
 }
-​
+
 function check() {
-​
+
   var question1 = document.trivia.question1.value;
   var question2 = document.trivia.question2.value;
   var question3 = document.trivia.question3.value;
@@ -34,29 +35,51 @@ function check() {
   var question8 = document.trivia.question8.value;
   var question9 = document.trivia.question9.value;
   var question10 = document.trivia.question10.value;
-​
+
   var correct = 0;
-​
+  var incorrect = 0;
+
   if (question1 == "Sweet") {
     correct++;
+  }
+  else{
+    incorrect++;
   }
   if (question2 == "Calico") {
     correct++;
   }
+  else{
+    incorrect++;
+  }
   if (question3 == "True") {
     correct++;
+  }
+  else{
+    incorrect++;
   }
   if (question4 == "Smell") {
     correct++;
   }
+  else{
+    incorrect++;
+  }
   if (question5 == "A Tom") {
     correct++;
+  }
+  else{
+    incorrect++;
   }
   if (question6 == "Left") {
     correct++;
   }
+  else{
+    incorrect++;
+  }
   if (question7 == "70%") {
     correct++;
+  }
+  else{
+    incorrect++;
   }
   if (question8 == "30-50%") {
     correct++;
@@ -64,76 +87,94 @@ function check() {
   if (question9 == "A Kindle") {
     correct++;
   }
+  else{
+    incorrect++;
+  }
   if (question10 == "Napoleon Bonaparte") {
     correct++;
   }
-​
+  else{
+    incorrect++;
+  }
+
   var commentary = ["You're the cat's meow!  Purrfect!", "Radiclaw!", "Purrhaps you have a feline fur-end!", "You seem to be furmiliar with cats!", "Maybe you're just furgetful.", "It seems as if some of these questions were purrplexing to you.", "Are you unfurmiliar with cats?", "How unfurtonate!", "How clawful!", "Just apawling!", "What a catastrophe!"];
   var gifs = ["assets/images/cat10.gif", "assets/images/cat9.gif", "assets/images/cat8.gif", "assets/images/cat7.gif", "assets/images/cat6.gif", "assets/images/cat5.gif", "assets/images/cat4.gif", "assets/images/cat3.gif", "assets/images/cat2.gif", "assets/images/cat1.gif", "assets/images/cat0.gif"];
   var score;
-​
+
   if (correct < 1) {
-    score = 10;
-  }
-​
+    gifs = 0;
+    commentary = 10;
+  
+
   if (correct == 1) {
-    score = 9;
+    gifs = 1;
+    commentary = 9;
   }
-​
+
   if (correct == 2) {
-    score = 8;
+    gifs = 2;
+    commentary = 8;
   }
-​
+
   if (correct == 3) {
-    score = 7;
+    gifs = 3;
+    commentary = 7;
   }
-​
+
   if (correct == 4) {
-    score = 6;
+    gifs = 4;
+    commentary = 6;
   }
-​
+
   if (correct == 5) {
-    score = 5;
+    gifs = 5;
+    commentary = 5;
   }
-​
+
   if (correct == 6) {
-    score = 4;
+    gifs = 6;
+    commentary = 4;
   }
-​
+
   if (correct == 7) {
-    score = 3;
+    gifs = 7;
+    commentary = 3;
   }
-​
+
   if (correct == 8) {
-    score = 2;
+    gifs = 8;
+    commentary = 2;
   }
-​
+
   if (correct == 9) {
-    score = 1;
+    gifs = 9;
+    commentary = 1;
   }
-​
+
   if (correct == 10) {
-    score = 0;
+    gifs = 10;
+    commentary = 0;
   }
-​
+
   setInterval(ctdwn, 1000);
   function ctdwn() {
     document.getElementById("time").innerText
     time--;
   }
   document.getElementById("after_submit").style.visibility = "visible";
-  document.getElementById("commentary").innerHTML = commentary[score];
+  document.getElementById("commentary").innerHTML = commentary;
   document.getElementById("number_correct").innerHTML = "Out of ten questions, you got " + correct + " of them right!";
+  document.getElementById("number_incorrect").innerHTML = +incorrect+ " answers were wrong."
   document.getElementById("gifs").src = gifs[score];
 }
-​
+
 document.getElementById("startGame").addEventListener("click", function () {
   runTimer();
   document.getElementById("after_submit").style.display = "none";
   document.getElementById("questionsDiv").style.display = "block";
   document.getElementById("startPage").style.display = "none";
 });
-​
+
 document.getElementById("subButton").addEventListener("click", function () {
   clearInterval(timer);
   check();
